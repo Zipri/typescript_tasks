2acf448c65bf6eaf950ca8b64daf862c8b79577c
Zipri <kirill.serov.00@mail.ru>
2022-10-06 20:29:15 +0300

    some fixed

--- src/common/Loader.tsx
+++ src/common/Loader.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import './Loader.css'
 
 const Loader = () => {
-    return <>
+    return <div style={{display: "flex", justifyContent: "center"}}>
         <div className="loadingio-spinner-eclipse-djk8r21pc2w">
             <div className="ldio-bxtaolevnoj">
                 <div/>
             </div>
         </div>
-    </>
+    </div>
 };
 
 export default Loader;
